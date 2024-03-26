11fd6c6e85a22d4d26f8e7970130d8ee40551e88
RaevenV <villarelr@gmail.com>
2024-03-26 22:08:46 +0700

    updated formInput1 + search feature added

--- frontend/src/components/inputTB.jsx
+++ frontend/src/components/inputTB.jsx
@@ -3,6 +3,24 @@ import "./inputTB.css";
 
 function InputTB(props) {
   const [content, setContent] = useState("");
+
+  const handleInputChange = (event) => {
+    const value = event.target.value;
+    setContent(value);
+
+    if (props.handleSKUChange) {
+      props.handleSKUChange(value);
+    } else if (props.handleTanggalChange) {
+      props.handleTanggalChange(value);
+    } else if (props.handleLineChange) {
+      props.handleLineChange(value);
+    } else if (props.handleQTChange) {
+      props.handleQTChange(value);
+    } else if (props.handleBuyerChange) {
+      props.handleBuyerChange(value);
+    }
+  };
+
   return (
     <>
       <div className="tb-wrapper">
@@ -11,9 +29,10 @@ function InputTB(props) {
           <div className="tb-sym">:</div>
         </div>
         <input
-          placeholder = {props.placeholder}
+          placeholder={props.placeholder}
           type="text"
           className="tb-input"
+          onChange={handleInputChange}
         />
       </div>
     </>
